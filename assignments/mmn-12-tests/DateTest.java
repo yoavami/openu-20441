89be08a258bcaa3451a68482c4e4ccba8654a696
Yoav Amit <yoavamit@icloud.com>
2023-04-16 15:47:06 -0400

    Add more tests

---
+++ assignments/mmn-12-tests/DateTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateTest {
+
+    @Test
+    void getDay() {
+    }
+
+    @Test
+    void setDay() {
+    }
+
+    @Test
+    void getMonth() {
+    }
+
+    @Test
+    void setMonth() {
+    }
+
+    @Test
+    void getYear() {
+    }
+
+    @Test
+    void setYear() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void before() {
+    }
+
+    @Test
+    void after() {
+    }
+
+    @Test
+    void difference() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void tomorrow() {
+    }
+}
