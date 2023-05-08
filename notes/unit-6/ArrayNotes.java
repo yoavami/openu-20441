a4ddcc0e31f8c241637e270371bf40ec87ff1510
Yoav Amit <yoavamit@icloud.com>
2023-05-08 18:46:04 -0400

    chore: Update notes

--- notes/unit-6/ArrayNotes.java
+++ notes/unit-6/ArrayNotes.java
@@ -1,13 +1,9 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArrayNotes {
     public static void main(String[] args) {
-        int[] array = new int[5];
-
-        for (int i = 1; i < array.length + 1; i++) {
-            array[i - 1] = i;
-        }
-        System.out.println(Arrays.toString(array));
+        
     }
 }
